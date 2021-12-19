7bdd15d35d49135dd2139d67ddc26c44c3c1ad5a
EJikia <eka.jikia18@gmail.com>
2021-12-19 20:28:22 +0400

    user page added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var loggedInUser = {};
 /*Login & Registration Pages*/
 function openLogInForm() {
     disappearForm("login_form", "register_form");
@@ -10,57 +11,64 @@ function openRegistrationForm() {
 }
 function disappearForm(elementToShow, elementToDisappear) {
 
-    document.getElementById("main_container").style.opacity = "0.3";
+    document.getElementById("homepage_container").style.opacity = "0.3";
     if (document.getElementById(elementToDisappear).style.display !== "none") {
         document.getElementById(elementToDisappear).style.display = "none";
     }
     let element = document.getElementById(elementToShow);
-    element.style.display = "flex"
+    element.style.display = "flex";
 }
-/*================= register user ===================*/
+
+
+/*================= user register  ===================*/
 let register_form = document.getElementById("register_form");
-register_form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let formElements = e.target.elements;
-    let userObj = {};
-    for (let element of formElements) {
-        if (element.tagName === "INPUT") {
-            userObj[element.name] = element.value;
-        }
-    }
-    if (userObj.fname == "" || userObj.lname == "" || userObj.email == "" || userObj.password == "") {
-        alert("Please fill all required fields!")
-        return;
-    }
-    if (userObj.password !== userObj.re_password) {
-        alert("Passwords Don't Match");
-        return;
-    }
-
-    else {
-        let usersArrayString = localStorage.getItem("users");
-        let parsedArray = JSON.parse(usersArrayString);
-
-        if (!parsedArray) {
-            parsedArray = [];
-        }
-
-        for (let user of parsedArray) {
-            if (user.email === userObj.email) {
-                alert("USER ALREADY EXISTS")
-                return;
+if (register_form != null) {
+    register_form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let formElements = e.target.elements;
+        let userObj = {};
+        for (let element of formElements) {
+            if (element.tagName === "INPUT") {
+                userObj[element.name] = element.value;
             }
         }
+        if (userObj.fname == "" || userObj.lname == "" || userObj.email == "" || userObj.password == "") {
+            alert("Please fill all required fields!")
+            return;
+        }
+        if (userObj.password !== userObj.re_password) {
+            alert("Passwords Don't Match");
+            return;
+        }
 
-        parsedArray.push(userObj)
-        let newArrayString = JSON.stringify(parsedArray);
-        localStorage.setItem("users", newArrayString);
-        alert("User Registered");
-        openLogInForm();
+        else {
+            let usersArrayString = localStorage.getItem("users");
+            let parsedArray = JSON.parse(usersArrayString);
 
-    }
-});
-/*================= register user ===================*/
+            if (!parsedArray) {
+                parsedArray = [];
+            }
+
+            for (let user of parsedArray) {
+                if (user.email === userObj.email) {
+                    alert("USER ALREADY EXISTS")
+                    return;
+                }
+            }
+
+            parsedArray.push(userObj)
+            let newArrayString = JSON.stringify(parsedArray);
+            localStorage.setItem("users", newArrayString);
+            alert("User Registered");
+            openLogInForm();
+
+        }
+    });
+}
+
+/*================= user register  ===================*/
+
+/*================= user login ===================*/
 function login() {
     let login_form = document.getElementById("login_form");
     let email = login_form.querySelector("input[name='email']");
@@ -72,35 +80,98 @@ function login() {
         usersObjects = [];
     }
 
-    console.log(email.value, password.value);
-
-    for (let user of usersObjects) {
-        console.log(user);
-        if (user.email === email.value && user.password === password.value) {
-            password.style.border = "none";
-            email.style.border = "none";
-            document.getElementById("login_form").style.display = "none";
-            document.getElementsByClassName("header-btn")[0].style.display = "none";
-            document.getElementsByClassName("header-btn")[1].style.display = "none";
-            let username = document.getElementById("username");
-            username.style.display = "inline-block";
-            username.textContent = user.fname;
-            let logout = document.getElementById("logout-btn");
-            logout.style.display = "inline-block";
-            document.getElementById("main_container").style.opacity = "1";
-            return;
-        }
-        if (user.email === email.value && user.password !== password.value) {
-            alert("Incorrect Parameters");
-            password.style.border = "solid #FF0000";
-            return;
-        }
-
+    let user = usersObjects.find(x => x.email === email.value);
+    if (user == null) {
+        password.style.border = "solid #FF0000";
+        email.style.border = "solid #FF0000";
+        alert("Incorrect parameters");
+        return;
     }
+    if (user.password != password.value) {
+        alert("Incorrect password");
+        console.log(user.password,password.value);
+        password.style.border = "solid #FF0000";
+        return;
+    }
+    password.style.border = "none";
+    email.style.border = "none";
+    document.getElementById("login_form").style.display = "none";
+    document.getElementsByClassName("header-btn")[0].style.display = "none";
+    document.getElementsByClassName("header-btn")[1].style.display = "none";
+    loggedInUser = { ...user };
+    let username = document.getElementById("username");
+    username.style.display = "inline-block";
+    username.textContent = user.fname;
+    let logout = document.getElementById("logout-btn");
+    logout.style.display = "inline-block";
+    document.getElementById("homepage_container").style.opacity = "1";
 
-    password.style.border = "solid #FF0000";
-    email.style.border = "solid #FF0000";
+};
+/*================= user login ===================*/
 
-    alert("Incorrect Parameters");
+/*================= user log out ===================*/
+function logout() {
 
-}
+    document.getElementsByClassName("header-btn")[0].style.display = "inline-block";
+    document.getElementsByClassName("header-btn")[1].style.display = "inline-block";
+    let username = document.getElementById("username");
+    username.style.display = "none";
+    username.textContent = "";
+    let logout = document.getElementById("logout-btn");
+    logout.style.display = "none";
+
+};
+
+/*================= user log out ===================*/
+
+function showPassword() {
+    let password = document.getElementById("userInfo_password");
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+};
+
+function loadUserPage() {
+    document.getElementById("homepage_container").style.display = "none";
+    document.getElementById("userInfo_container").style.display = "flex";
+    document.getElementById("userInfo_firstname").value = loggedInUser.fname;
+    document.getElementById("userInfo_lastname").value = loggedInUser.lname;
+    document.getElementById("userInfo_email").value = loggedInUser.email;
+    document.getElementById("userInfo_password").value = loggedInUser.password;
+};
+
+function changeInfo() {
+    document.getElementById("userInfo_firstname").disabled = false;
+    document.getElementById("userInfo_lastname").disabled = false;
+    document.getElementById("userInfo_password").disabled = false;
+    document.getElementById("saveUpdatedUserInfo").disabled = false;
+    document.getElementById("changeUserInfo").disabled = true;
+};
+
+function updateInfo() {
+    let usersString = localStorage.getItem("users");
+    let usersObjects = JSON.parse(usersString);
+    let user = usersObjects.find(x => x.email === loggedInUser.email);
+    user.fname = document.getElementById("userInfo_firstname").value;
+    user.lname = document.getElementById("userInfo_lastname").value;
+    user.password = document.getElementById("userInfo_password").value;
+    var newArrayString = JSON.stringify(usersObjects);
+    localStorage.setItem("users", newArrayString);
+    document.getElementById("changeUserInfo").disabled = false;
+    document.getElementById("userInfo_firstname").disabled = true;
+    document.getElementById("userInfo_lastname").disabled = true;
+    document.getElementById("userInfo_password").disabled = true;
+    document.getElementById("saveUpdatedUserInfo").disabled = true;
+};
+
+/*================= user log out ===================*/
+
+/*================= Hotels ===================*/
+// class Hotel {
+//     constructor(name, address, numberOfStars, info, numberOfRooms, freeRooms)
+// }
+
+
+/*================= Hotels ===================*/
